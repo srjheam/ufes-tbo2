8fa0a8d3f7dc88ad72af5b46608f0d923faa0235
Jheam Storch Ross <jheam.ross@gmail.com>
2023-10-30 23:51:05 -0300

    fix rtt

--- src/rttlib/include/rttlib/fileOperations.h
+++ src/rttlib/include/rttlib/fileOperations.h
@@ -6,7 +6,6 @@
 #include "rttlib/graph.h"
 
 int readGraphFromFile(char *filename, Graph **graph, int *qtyS, int *qtyC, int *qtyM, int **arr_S_ids, int **arr_C_ids, int **arr_M_ids);
-void printGraphToFile(char *filename, Graph *graph);
 
 void printRttRatioToFile(FILE *output_file, Heap *rttInfos);
 
--- src/rttlib/rttInfo.c
+++ src/rttlib/rttInfo.c
@@ -1,30 +1,34 @@
 #include "rttlib/rttInfo.h"
 
+double __rtt(double **distances, int a, int b) {
+    return distances[a][b] + distances[b][a];
+}
+
 Heap *rttinfo_calc(double **distances, int qtyS, int qtyC, int qtyM, int *arr_S_ids, int *arr_C_ids, int *arr_M_ids) {
     Heap *rttInfos = heap_init(MIN_HEAP, qtyS * qtyC, sizeof(RttInfo), NULL);
 
     for (int i = 0; i < qtyS; i++)
     {
-        int src_id = arr_S_ids[i];
+        int server_id = arr_S_ids[i];
         for (int j = 0; j < qtyC; j++)
         {
-            int dest_id = arr_C_ids[j];
+            int client_id = arr_C_ids[j];
 
-            double rtt = distances[src_id][dest_id];
+            double rtt = __rtt(distances, server_id, client_id);
 
             double rtt_star = __DBL_MAX__;
             for (int k = 0; k < qtyM; k++)
             {
                 int monitor_id = arr_M_ids[k];
 
-                double rtt_m = distances[src_id][monitor_id] + distances[monitor_id][dest_id];
+                double rtt_m = __rtt(distances, server_id, monitor_id) + __rtt(distances, monitor_id, client_id);
                 if (rtt_m < rtt_star)
                     rtt_star = rtt_m;
             }
 
-            RttInfo rttInfo = {.c_id = src_id, .s_id = dest_id};
+            RttInfo rttInfo = {.c_id = server_id, .s_id = client_id};
 
-            heap_push(rttInfos, &rttInfo, rtt_star / rtt);
+            heap_push(rttInfos, &rttInfo, (rtt_star / rtt));
         }
     }    
 
